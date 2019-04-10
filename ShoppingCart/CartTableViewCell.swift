533bed5c3e2b04878dd7088a8d6be3114aab3f27
anacamargos <alcvlara@sga.pucminas.br>
2019-04-10 17:08:55 -0300

    First commit

--- ShoppingCart/CartTableViewCell.swift
+++ ShoppingCart/CartTableViewCell.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class CartTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var productImageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
--- ShoppingCart/CartViewController.swift
+++ ShoppingCart/CartViewController.swift
@@ -8,23 +8,28 @@
 
 import UIKit
 
-class CartViewController: UIViewController {
+class CartViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cartCell", for: indexPath) as! CartTableViewCell
+        
+        cell.productImageView.image = UIImage(named: "img_ipad")
+        cell.productImageView.backgroundColor = UIColor.red
+
+        return cell
+    }
 
 }
