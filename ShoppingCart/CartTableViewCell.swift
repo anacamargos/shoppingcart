c3ba3476d5e8cc38cb7b7c2532e0a5727d48c9d4
anacamargos <alcvlara@sga.pucminas.br>
2019-04-10 17:04:57 -0300

    First commit

---
+++ ShoppingCart/CartTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CartTableViewCell.swift
+//  ShoppingCart
+//
+//  Created by Ana Leticia Camargos on 10/04/19.
+//  Copyright © 2019 Ana Leticia Camargos. All rights reserved.
+//
+
+import UIKit
+
+class CartTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
