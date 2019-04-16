42ec6cef39b2dd4b4bc70130d0f5e5f142d429e9
anacamargos <alcvlara@sga.pucminas.br>
2019-04-16 17:32:17 -0300

    Updating Shipping Buttons

--- ShoppingCart/Controller/CheckoutViewController.swift
+++ ShoppingCart/Controller/CheckoutViewController.swift
@@ -11,14 +11,57 @@ import UIKit
 class CheckoutViewController: UIViewController {
     
     @IBOutlet weak var totalValueLabel: UILabel!
+    @IBOutlet weak var expressButton: UIButton!
+    @IBOutlet weak var standardButton: UIButton!
     
     var totalValue : String = ""
+    var originalTotalValue: Double = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.totalValueLabel.text = "\(totalValue)"
+        let valueInDouble = Double(totalValue.substring(with: 1..<totalValue.count))
+        originalTotalValue = valueInDouble!
+        
+        standardButton.isEnabled = false
 
     }
     
+    @IBAction func expressSelection(_ sender: Any) {
+        standardButton.isEnabled = true
+        expressButton.isEnabled = false
+        
+        let valueInDouble = Double(totalValue.substring(with: 1..<totalValue.count))
+        self.totalValueLabel.text = "$\(valueInDouble! + 15)"
+    }
+    
+    @IBAction func standardSelection(_ sender: Any) {
+        standardButton.isEnabled = false
+        expressButton.isEnabled = true
+        
+        self.totalValueLabel.text = "$\(originalTotalValue)"
+    }
+}
+
+extension String {
+    func index(from: Int) -> Index {
+        return self.index(startIndex, offsetBy: from)
+    }
+    
+    func substring(from: Int) -> String {
+        let fromIndex = index(from: from)
+        return substring(from: fromIndex)
+    }
+    
+    func substring(to: Int) -> String {
+        let toIndex = index(from: to)
+        return substring(to: toIndex)
+    }
+    
+    func substring(with r: Range<Int>) -> String {
+        let startIndex = index(from: r.lowerBound)
+        let endIndex = index(from: r.upperBound)
+        return substring(with: startIndex..<endIndex)
+    }
 }
